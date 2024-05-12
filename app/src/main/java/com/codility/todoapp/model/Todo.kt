71f378104294c945faf2df056d4bf25fd9937ba5
YomithG <165700401+YomithG@users.noreply.github.com>
2024-05-12 20:48:32 +0530

    Initial commit

---
+++ app/src/main/java/com/codility/todoapp/model/Todo.kt
@@ -0,0 +1,13 @@
+package com.codility.todoapp.helper
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "todoTable")
+data class Todo(
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0,
+    val title: String,
+    val description: String,
+    val timestamp: String
+)
